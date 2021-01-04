6f3c2472300dfd6beb1731156e663c4cb0188967
Fukiwake <tak0203753@gmail.com>
2021-01-04 14:06:47 +0000

    [Finish] 2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,13 +4,9 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
 
-  # 以下を追加
   def create
-    # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # ３. トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
   
@@ -21,9 +17,18 @@ class TodolistsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
 
   private
-  # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
