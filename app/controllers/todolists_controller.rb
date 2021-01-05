beac805cba7a4cd5d87af54f6f527acf66d46060
Fukiwake <tak0203753@gmail.com>
2021-01-05 06:28:37 +0000

    [Finish] 2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,7 +9,7 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
   end
   def index
-    @todolists = List.all
+    @lists = List.all
   end
   def show
     @list = List.find(params[:id])
@@ -22,6 +22,12 @@ class TodolistsController < ApplicationController
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
+  end
+
 
   private
   def list_params
